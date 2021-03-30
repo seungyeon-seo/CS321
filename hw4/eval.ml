c4d2e6e3826adcbe4fba9738948032499782432a
seungyeon-seo <kcd6256@gmail.com>
2021-03-30 15:51:42 +0900

    implement substitution and stepv

--- hw4/eval.ml
+++ hw4/eval.ml
@@ -1,6 +1,7 @@
 (*
  * Call-by-value reduction   
  *)
+open Uml
 
 exception NotImplemented 
 exception Stuck
@@ -31,24 +32,44 @@ let isFreeVariable v e =
 
 (* let rec stepv e = raise NotImplemented *)
 let rec stepv e =
-   let rec substitution e x e' =
+  let swapExp e x y =
+    match e with 
+    | Var x -> e
+    | _ -> e
+    (* TODO: implement swap function *)
+  in
+  let rec substitution e x e' =
   match e' with
   | Var y ->
-    if x=y then stepv e else y
+    if x=y then e else Var y
   | Lam (y, e1) ->
-    if x=y then 
+    if x=y then Lam (y, e1)
+    else if isFreeVariable y e then
+      let z = getFreshVariable x in Lam (z, (substitution e x (swapExp e1 y (Var(z)))))
+    else Lam (y, (substitution e x e1))
   | App (e1, e2) -> 
-    stepv (App ((substitution e x e1) (substitution e x e2)))
-  in      
+    (App ((substitution e x e1), (substitution e x e2)))
+  in
   match e with
-  | Var x -> x
-  | Lam (x, e') -> 
+  | Var x -> raise Stuck
+  | Lam (x, e') ->
+    (match e' with
+    | Var y -> e'
+    | Lam (y, ee) -> Lam (y, (stepv ee))
+    | App (e1, e2) -> Lam (x, e2))
+  | App (e1, e2) -> 
+    let e1' = stepv e1 in
+    let e2' = stepv e2 in
+    match (e1', e2') with
+    | ((Lam (a, e11)), Var b) -> substitution e2' a e11 (* App: B-reduction *)
+    | (Var a, _) -> Lam (a, e2') (* Arg *)
+    | (_, _) -> App (e1', e2) (* Lam *)
 
 
 (*
  * implement a single step with reduction using the call-by-name strategy.
  *)
-let rec stepn e = raise NotImplemented         
+let rec stepn e = stepv e    
 
 let stepOpt stepf e = try Some (stepf e) with Stuck -> None
 
