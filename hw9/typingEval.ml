ea1c88e5b1a94af753bf4d9ebd723332aadf141a
seungyeon-seo <kcd6256@gmail.com>
2021-05-09 18:13:06 +0900

    T-Var, T-Field

--- hw9/typingEval.ml
+++ hw9/typingEval.ml
@@ -19,8 +19,42 @@ let rec isSubClass a b l =
   else if super = b then true
   else try isSubClass (findCdec super l) b with | NotFound -> false
 
+let rec fields c clDlist =
+  match clDlist with
+  | [] -> rasie NotFound
+  | (cls, _, flist, _, _)::t ->
+    if c = cls then flist else fields c t
+
+let rec mtype m c0 clDlist =
+  match clDlist with
+  | [] -> raise NotFound
+  | (cls, _, _, _, mlist)::t ->
+    if c0 = c then 
+  (* match mlist with
+  | [] -> raise NotFound
+  | (ty, mname, plist, body)::t ->
+    if c = mname then (plist, ty) else getTypeMethod c t *)
+
 (* Fjava.program -> Fjava.typ *)
-let typeOf p =
+let rec typeOf p =
+  let (clDlist, exp) = p in
+  match exp with
+  (* T-Var *)
+  | Var v -> v
+  | Field (e', s) ->
+    (* T-Field *)
+    let flist = fields (typeOf (clDlist, e')) clDlist in
+    List.asso s (List.map (fun (x,y) -> (y,x)) flist)
+  | Method (e0, m, e_) ->
+    (* T-Invk *) (* ?? D->C가 뭘까? *)
+    let c0 = typeOf (clDlist, e0) in
+    let mt = mtype m c0 in
+    (* try let (d_, c) = getTypeMethod c0 flist with | NotFound -> raise TypeError in
+    let c_ = in *)
+    
+  | New (t, l) -> 
+
+  | Cast (t, e') ->
 
 
 let step p = raise NotImplemented
