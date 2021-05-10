f2bd6c329a61a67e569ac7e0a72c010e444ab615
seungyeon-seo <kcd6256@gmail.com>
2021-05-10 21:51:42 +0900

    implement mtype

--- hw9/typingEval.ml
+++ hw9/typingEval.ml
@@ -13,10 +13,25 @@ let rec getCls c clsDecl =
     let (cls, _, _, _, _) = h in
     if cls = c then h else getCls c t
 
-(* classname -> classname -> bool *)
+(* method name -> methodDecl list -> methodDecl *)
+let rec getMthd m mlist =
+  match mlist with
+  | [] -> raise NotFound
+  | h::t ->
+    let (_, mn, _, _) = h in
+    if mn = m then h else getMthd m t
+
+(* classname -> classname -> classDecl list -> bool *)
 let rec isSubclass a b clsDecl =
   let (_, sup, _, _, _) = try getCls a clsDecl with | NotFound -> false in
-  if sup = b then true else isSubClass sup b clsDecl
+  if sup = b then true else isSubclass sup b clsDecl
+
+(* classname list -> classname list -> classDecl list -> bool *)
+let rec isSubclass2 a_ b_ clsDecl =
+  match (a_, b_) with
+  | ([], []) -> true
+  | (a::at, b::bt) ->
+    if isSubclass a b clsDecl then isSubclass2 at bt clsDecl else false
 
 (* classname -> classDecl list -> fields: (classname, fieldname) list *)
 let rec fields c clsDecl =
@@ -25,6 +40,7 @@ let rec fields c clsDecl =
   (c_, f_)::flist
 
 (* check if flist in plist *)
+(* (typ*string) list -> (typ*string) list -> bool *)
 let rec checkList plist flist =
   match flist with
   | [] -> true
@@ -33,8 +49,27 @@ let rec checkList plist flist =
     then (if (List.assoc a plist) = b then checkList plist t else false)
     else false
 
+(* methodname -> classname -> (typlist of parameters, return type)*)
+let rec mtype m c clsDecl = 
+  let (_, d, flist, k, mlist) = try getCls c clsDecl with | NotFound -> raise NotFound in
+  let (b, _, plist, e) = try getMthd m mlist
+                        with | NotFound -> mtype m d clsDecl in
+  let (b_, _) = List.split plist in
+  (b_, b)
+
 (* exp -> env(string * typ list) -> classDecl list *)
-let exp_typing e env clsDecl = raise NotImplemented
+(* let typeofexp e env clsDecl =
+  match e with
+  | Var x ->
+    (* T-Var *)
+    try List.assoc x env with | Not_found -> raise NotFound
+  | Field (e0, f) ->
+    try let c0 = List.assoc e0 env with | Not_found -> rasie NotFound in
+    let (d_, c) = mtype m c0 clsDecl in
+    typeofexps e_ env
+  | Method (e0, m, e_) ->
+  | New (c, e_) ->
+  | Cast (c, e0) -> *)
 
 (* methodDecl -> classDecl -> classDecl list -> bool *)
 let t_method m' cls clsDecl =
@@ -42,7 +77,7 @@ let t_method m' cls clsDecl =
   let (c, d, _, _, _) = cls in
   if override m d, (c_, c0) then
   (let env = (x_, c_)::(m, c) in
-  let e0t = exp_typing e0 env clsDecl in
+  let e0t = typeofexp e0 env clsDecl in
   isSubclass e0t c0
   )
   else false
@@ -54,7 +89,7 @@ let rec t_methods mlist c clsDecl =
   | m::t ->
     if t_method m c clsDecl then t_methods t c clsDecl else false
 
-(* classDecl -> bool *)
+(* classDecl -> classDecl list -> bool *)
 let t_class cls clsDecl = 
   let (c, d, flist, k, m_) = cls in
   let (c_, f_) = List.split flist in
@@ -64,7 +99,18 @@ let t_class cls clsDecl =
     let flist = fields d clsDecl in
     if checkList plist flist then t_methods mlist cls clsDecl else false
 
-let typeOf p = raise NotImplemented
+(* Fjava.program -> Fjava.typ *)
+let typeOf p = 
+  let rec checkClass clsDecl =
+    match clsDecl with
+    | [] -> true
+    | h::t -> if t_class h clsDecl then checkClass t else false
+  in
+  let (clsDecl, exp) = p in
+  (* TODO: env? *)
+  if checkClass then try typeofexp exp env clsDecl with | NotFound -> raise TypeError
+  else raise TypeError
+  
 
 let step p = raise NotImplemented
 
